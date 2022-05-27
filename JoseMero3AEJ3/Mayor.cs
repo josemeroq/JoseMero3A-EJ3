20cffcda9d5580116f44d8b2150709f9f4b8f213
josemeroq <93845854+josemeroq@users.noreply.github.com>
2022-05-26 19:24:29 -0500

    Add files via upload

---
+++ JoseMero3AEJ3/Mayor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JoseMero3AEJ3
+{
+    public class Mayor
+    {
+        private int valorA { set; get; }
+        private int valorB { set; get; }
+        private int valorC { set; get; }
+
+        public Mayor(int valorA, int valorB, int valorC)
+        {
+            this.valorA = valorA;
+            this.valorB = valorB;
+            this.valorC = valorC;
+        }
+        public void proceso()
+        {
+            if (valorA > valorB && valorA > valorC)
+            {
+                Console.WriteLine("El numero " + valorA + " es mayor");
+            }
+            else if (valorB > valorA && valorB > valorC)
+            {
+                Console.WriteLine("El numero " + valorB + " es mayor");
+            }
+            else
+            {
+                Console.WriteLine("El numero " + valorC + " es mayor");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ JoseMero3AEJ3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace JoseMero3AEJ3
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                Mayor mayorn = new Mayor(6, 8, 14);
+                mayorn.proceso();
+            }
+
+        }
+    }
+
